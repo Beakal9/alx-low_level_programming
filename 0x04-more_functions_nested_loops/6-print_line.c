b59497ac916768be694021dac98961f64b395f8e
Beakal ketema <beakalk@yahoo.com>
2022-09-15 06:12:30 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,11 +6,16 @@
  */
 void print_line(int n)
 {
-	int x;
+	int x, y;
 
-	if (n <= 0)
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
 		_putchar('\n');
-	for (x = 0 x <= n; x++)
-		_putchar('_');
-	_putchar('\n');
+	}
 }
