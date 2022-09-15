b4dc1ada496642deaf471a42fc2fa4ef01bdcf05
Beakal ketema <beakalk@yahoo.com>
2022-09-15 07:22:07 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers -> printing more numbers
+ * more_numbers - printing 10 times the numbers since 0 up to 14
  *
- * Return: Always success
+ * Return: 10 times of the numbers since 0 up to 14
  */
 void more_numbers(void)
 {
@@ -11,10 +11,15 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y + '0');
+			if (y > 9)
+			{
+			_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
 		}
-		_putchar('\n');
+
+		_putchar('/n');
 	}
 }
