b839f34bb508ef1d870229861c0cfc558b7578cb
Beakal ketema <beakalk@yahoo.com>
2022-09-19 00:50:09 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#nclude "main.h"
+/**
+ * _puts - prnts a string
+ * @str: A pointer to an int that will be changd
+ *
+ * Return: void which means ou answer is correct
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str)
+	}
+	_putchar('\n');
+}
