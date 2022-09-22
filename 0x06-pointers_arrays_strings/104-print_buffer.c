ffbc035a6834eb2854b58429aa2b34536c303b73
Beakal ketema <beakalk@yahoo.com>
2022-09-22 03:16:22 -0700

    Task 6

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,15 +16,16 @@ void print_buffer(char *b, int size)
 	while (i < size)
 	{
 		if (i % 10 == 0)
-			printf(%08x: ", i);
-		for(j = i; j < i + 9; j += 2)
+			printf("%08x: ", i);
+		for (j = i; j < i + 9; j += 2)
 		{
-			if (j < size) && ((j + 1) < size))
+			if ((j < size) && ((j + 1) < size))
 				printf("%02x%02x: ", b[j], b[j + 1]);
 			else
 			{
 				while (++j <= i + 10)
 					printf(" ");
+				printf(" ");
 			}
 		}
 		for (j = i; j < i + 9 && j < size; j++)
