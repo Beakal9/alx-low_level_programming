47f67c7fee4c8048735c858b942ca69ea7e6ccac
Beakal ketema <beakalk@yahoo.com>
2022-09-30 12:31:53 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 	for (index = 1; index < argc; index++)
 	{
 		multiplication = multiplication * atoi(argv[index]);
-		return (0);
 	}
+	printf("%d\n", multiplication);
+	return (0);
 }
