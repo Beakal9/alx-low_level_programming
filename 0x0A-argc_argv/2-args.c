cb524b1a9897f954862868fc5f497fc110a5c9bd
Beakal ketema <beakalk@yahoo.com>
2022-09-30 07:18:33 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arg
+ * @argc: num of arg
+ * @argv: array of arg
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < arg; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
