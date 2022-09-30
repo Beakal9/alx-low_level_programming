f82b1429372ef3652746631e555537f8ade1fa65
Beakal ketema <beakalk@yahoo.com>
2022-09-30 07:29:08 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints two numbers
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+}
