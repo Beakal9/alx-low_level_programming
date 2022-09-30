9b0e35a6e1e06a224f0cb51df1586ce6c19fe782
Beakal ketema <beakalk@yahoo.com>
2022-09-30 06:57:58 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: arg count
+ * @argv: arg vector
+ */
+int main(int argc, char *argv[])
+{
+	pritf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
