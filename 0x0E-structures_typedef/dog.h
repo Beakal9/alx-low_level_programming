0acb45577155c14db4eb6f1274d0a19e042f701f
Beakal ketema <beakalk@yahoo.com>
2022-10-10 05:30:12 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 /**
  * struct dog - structure of a dog.
  * @name: name of a dog.
@@ -14,11 +14,11 @@ struct dog
 	char *owner;
 };
 
-typedef stuct dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif /*STRTYP*/
