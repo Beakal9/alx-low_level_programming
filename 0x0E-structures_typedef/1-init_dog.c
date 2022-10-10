c0c59555cd9ef5ea1f5f16fe4e9c15a35f218d3a
Beakal ketema <beakalk@yahoo.com>
2022-10-10 05:03:04 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initializes of var struct
+ * @d: pointers to struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Return: void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
