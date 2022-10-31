51e5de3e3864f9a4a0e36dcf4cc876b92518789a
Beakal ketema <beakalk@yahoo.com>
2022-10-31 13:43:01 -0700

    Task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -39,3 +39,135 @@ void check_elf(unsigned char *e_ident)
 		}
 	}
 }
+
+/**
+ * print_magic - print magic num
+ * @e_ident: pointer to array
+ *
+ * Description: numbers
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int index;
+
+	printf(" Magic: ");
+
+	for (index = 0; index < EI_NIDENT; index++)
+	{
+		printf("%02x", e_ident[index]);
+
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+/**
+ * print_class - prints elf
+ * @e_ident: pointer
+ */
+void print_class(unsigned char *e_ident)
+{
+	printf(" class:                       ");
+
+	switch (e_ident[EI_CLASS])
+	{
+		case ELFCLASSNONE:
+			printf("none\n");
+			break;
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+/**
+ * print_data - print data
+ * @e_ident: pointer to array
+ */
+void print_data(unsigned char *e_ident)
+{
+	printf(" Data:              ");
+
+	switch (e_ident[EI_DATA])
+	{
+		case ELFDATANONE:
+			printf("none\n");
+			break;
+		case ELFDATA2LSB:
+			priintf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			priintf("2's complement, big endian\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+/**
+ * print_version - print version
+ * @e_ident: pointer
+ */
+void print_version(unsigned char *e_ident)
+{
+	printf(" version:              %d",
+		e_ident[EI_VERSION]);
+
+	switch (e_ident[EI_VERSION])
+	{
+		case EV_CURRENT:
+			printf(" (current)\n");
+			break;
+		default:
+			printf("\n");
+	}
+}
+/**
+ * print_osabi - prints os/ABI
+ * @e_ident: pointer to array
+ */
+void print_osabi(unsigned char *e_ident)
+{
+	printf("  OS/ABI:              ");
+
+	switch (e_ident[EI_OSABI])
+	{
+		case ELFOSABI_NONE:
+			printf("UNIX - System V\n");
+			break;
+		case ELFOSABI_HPUX:
+			printf("UNIX - HP-UX\n");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD\n");
+			break;
+		case ELFOSABI_LINUX:
+			printf("UNIX - Linux\n");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris\n");
+			break;
+		case ELFOSABI_IRIX:
+			printf("UNIX - IRIX\n");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("UNIX - FreeBSD\n");
+			break;
+		case ELFOSABI_TRU64:
+			printf("UNIX - TRU64\n");
+			break;
+		case ELFOSABI_ARM:
+			printf("ARM\n");
+			break;
+		case ELFOSABI_STANDALONE:
+			printf("Standalone App\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", e_ident[EI_OSABI]);
+	}
+}
+
